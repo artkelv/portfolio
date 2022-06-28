6cceb19e9d5e8d1ca1ace0774f0a0ad452da7875
Arthut Kelvim <kelvimarthur@gmail.com>
2022-06-27 23:02:35 -0300

    Inicio da estrutura do meu portfolio

---
+++ portfolio-arthur/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header";
+import { GlobalStyled } from "./GlobalStyled";
+
+function App() {
+  return (
+    <div>
+        <GlobalStyled/>      
+        <Header/>
+    </div>     
+    )
+}
+
+export default App;
