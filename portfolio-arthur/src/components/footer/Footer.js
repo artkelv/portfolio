47603b153f8af0cb474a19fc848299df0a733d4b
Arthut Kelvim <kelvimarthur@gmail.com>
2022-06-30 19:05:08 -0300

    Footer do projeto feito.

--- portfolio-arthur/src/components/footer/Footer.js
+++ portfolio-arthur/src/components/footer/Footer.js
@@ -1,10 +1,40 @@
-import { Pgraph, Sat } from "./styled"
+import { FaLinkedin, FaGithub, FaWhatsapp, FaRegEnvelope, FaRegHeart, FaHeart } from "react-icons/fa"
+import { FooterSection, TitleContact, ContentFooter, FaIcons, Form, InputName, InputEmail, InputTel, InputTextarea, InputSubmit, EndSection, EndDesc } from "./styled"
+
+const figFa = {
+    fontSize:"50px",
+    margin:"5px"
+}
 
 const Footer = () => {
     return(
-        <Sat>
-            <Pgraph>hello</Pgraph>
-        </Sat>
+        <footer>
+            <FooterSection>
+                <TitleContact>Contato . . .</TitleContact>
+                <ContentFooter>
+                    <Form action="">
+                        <label htmlFor="">Nome</label>
+                        <InputName type="text"/>
+                        <label htmlFor="">Email</label>
+                        <InputEmail type="email"/>
+                        <label htmlFor="">Telefone</label>
+                        <InputTel type="tel"/>
+                        <label htmlFor="">Mensagem</label>
+                        <InputTextarea  id="" cols="30" rows="10"></InputTextarea>
+                        <InputSubmit type="submit" />
+                    </Form>
+                    <FaIcons>
+                        <FaLinkedin style={figFa}/>
+                        <FaGithub style={figFa}/>
+                        <FaWhatsapp style={figFa}/>
+                        <FaRegEnvelope style={figFa}/>
+                    </FaIcons>
+                </ContentFooter>
+            </FooterSection>
+            <EndSection>
+                <p>Feito com <FaRegHeart/> e <FaHeart/> por Arthur.</p>
+            </EndSection>
+        </footer>
     )
 }
 export default Footer
--- portfolio-arthur/src/components/footer/styled.js
+++ portfolio-arthur/src/components/footer/styled.js
@@ -1,9 +1,73 @@
 import styled from "styled-components";
+import backgroundFooter from "../../assets/back_footer.jpg"
+//Form, InputName, InputEmail, InputTel, InputTextarea, InputSubmit
 
-export const Sat = styled.div`
-    height: 50px;
-    background-color: black;
+
+export const FooterSection = styled.section`
+    color: #fff;
+    text-shadow: 2px 2px #111;
+    background-image: url(${backgroundFooter});
+    background-size: cover;
+    height: 100vh;
 `
-export const Pgraph = styled.p`
-    background-color: red;
+export const TitleContact = styled.h4`
+    font-size: 30px;
+    //background-color: red;
+    width: 50%;
+    margin: 0 auto;
+    text-align: left;
+    padding:50px 0px 50px 50px;
 ` 
+export const ContentFooter = styled.section`
+    display: flex;
+    justify-content: center;
+`
+export const FaIcons = styled.section`
+    border-left: 2px solid #fff;
+    padding-left: 50px;
+    margin: 0px 10px;
+`
+export const Form = styled.form`
+    display:flex;
+    flex-direction: column;
+    margin: 0px 10px;
+    
+`
+export const InputName = styled.input`
+    margin: 5px 0px;
+    border: 1px solid #fff;
+    background-color:rgba(192,192,192,0.3);
+`
+export const InputEmail = styled.input`
+    margin: 5px 0px;
+    border: 1px solid #fff;
+    background-color:rgba(192,192,192,0.3);
+`
+export const InputTel = styled.input`
+    margin: 5px 0px;
+    border: 1px solid #fff;
+    background-color:rgba(192,192,192,0.3);
+`
+export const InputTextarea = styled.textarea`
+    margin: 5px 0px;
+    border: 1px solid #fff;
+    background-color:rgba(192,192,192,0.3);
+`
+export const InputSubmit = styled.input`
+    cursor: pointer;
+    color: #fff;
+    background-color: transparent;
+    border: 1px solid #fff;
+    height: 30px;
+    &:hover{
+        background-color: #111;
+    }
+`
+export const EndSection = styled.section`
+    margin-top: -16px;
+    height: 50px;
+    background-color: #000;
+    color: white;
+    display: flex;
+    justify-content: center;
+`
